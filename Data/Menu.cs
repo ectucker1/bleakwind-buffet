b396d3858a7e3dbb871e4e77df360da7f064253f
Ethan Tucker <ectucker1@gmail.com>
2020-09-10 21:47:26 -0500

    Added the static Menu class which creates lists

---
+++ Data/Menu.cs
@@ -0,0 +1,119 @@
+﻿using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Sides;
+using System.Collections.Generic;
+
+/*
+* Author: Ethan Tucker
+* Class name: Entree.cs
+* Purpose: Static class used to list all menu items
+*/
+namespace BleakwindBuffet.Data
+{
+    /// <summary>
+    /// Static class with methods used to list all menu items
+    /// </summary>
+    public static class Menu
+    {
+        /// <summary>
+        /// Creates a list of all entrees on the menu
+        /// </summary>
+        /// <returns>A list of all entrees on the menu</returns>
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            List<IOrderItem> entrees = new List<IOrderItem>();
+            entrees.Add(new BriarheartBurger());
+            entrees.Add(new DoubleDraugr());
+            entrees.Add(new ThalmorTriple());
+            entrees.Add(new GardenOrcOmelette());
+            entrees.Add(new PhillyPoacher());
+            entrees.Add(new SmokehouseSkeleton());
+            entrees.Add(new ThugsTBone());
+            return entrees;
+        }
+
+        /// <summary>
+        /// Creates a list of all sides on the menu in every size
+        /// </summary>
+        /// <returns>A list of all sides on the menu</returns>
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            List<IOrderItem> sides = new List<IOrderItem>();
+            // Loop through each size by exploiting the fact that enums are integers
+            for (Size size = Size.Small; size <= Size.Large; size++)
+            {
+                sides.Add(new VokunSalad()
+                {
+                    Size = size
+                });
+                sides.Add(new FriedMiraak()
+                {
+                    Size = size
+                });
+                sides.Add(new MadOtarGrits()
+                {
+                    Size = size
+                });
+                sides.Add(new DragonbornWaffleFries()
+                {
+                    Size = size
+                });
+            }
+            return sides;
+        }
+
+        /// <summary>
+        /// Creates a list of all drinks on the menu in every size and flavor
+        /// </summary>
+        /// <returns>A list of all drinks on the menu</returns>
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            List<IOrderItem> drinks = new List<IOrderItem>();
+            // Loop through each size by exploiting the fact that enums are integers
+            for (Size size = Size.Small; size <= Size.Large; size++)
+            {
+                drinks.Add(new AretinoAppleJuice()
+                {
+                    Size = size
+                });
+                drinks.Add(new CandlehearthCoffee()
+                {
+                    Size = size
+                });
+                drinks.Add(new MarkarthMilk()
+                {
+                    Size = size
+                });
+                drinks.Add(new WarriorWater()
+                {
+                    Size = size
+                });
+                // Loop through each soda flavor
+                // Watermelon is the ultimate flavor and thus the highest
+                for (SodaFlavor flavor = SodaFlavor.Blackberry; flavor <= SodaFlavor.Watermelon; flavor++)
+                {
+                    drinks.Add(new SailorSoda()
+                    {
+                        Size = size,
+                        Flavor = flavor
+                    });
+                }
+            }
+            return drinks;
+        }
+
+        /// <summary>
+        /// Creates a list of every item on the menu, in all sizes and flavors
+        /// </summary>
+        /// <returns>A list of every item on the menu</returns>
+        public static IEnumerable<IOrderItem> FullMenu()
+        {
+            List<IOrderItem> items = new List<IOrderItem>();
+            items.AddRange(Entrees());
+            items.AddRange(Sides());
+            items.AddRange(Drinks());
+            return items;
+        }
+    }
+}
---
+++ DataTests/UnitTests/MenuTests.cs
@@ -0,0 +1,452 @@
+﻿/*
+ * Author: Ethan Tucker
+ * Class: MenuTests.cs
+ * Purpose: Test the Menu.cs class in the Data library
+ */
+using Xunit;
+
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Enums;
+
+namespace BleakwindBuffet.DataTests.UnitTests
+{
+    public class MenuTests
+    {
+        [Fact]
+        public void ShouldReturnCorrectEntreeList()
+        {
+            var entrees = Menu.Entrees();
+            // Run an assertion lambda for each entree
+            Assert.Collection(entrees,
+                item => Assert.IsType<BriarheartBurger>(item),
+                item => Assert.IsType<DoubleDraugr>(item),
+                item => Assert.IsType<ThalmorTriple>(item),
+                item => Assert.IsType<GardenOrcOmelette>(item),
+                item => Assert.IsType<PhillyPoacher>(item),
+                item => Assert.IsType<SmokehouseSkeleton>(item),
+                item => Assert.IsType<ThugsTBone>(item)
+            );
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectSideList()
+        {
+            var sides = Menu.Sides();
+            // Run an assertion lambda for each side
+            Assert.Collection(sides,
+                /* Small Sides */
+                item => {
+                    Assert.IsType<VokunSalad>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<FriedMiraak>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<MadOtarGrits>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<DragonbornWaffleFries>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                /* Medium Sides */
+                item => {
+                    Assert.IsType<VokunSalad>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<FriedMiraak>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<MadOtarGrits>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<DragonbornWaffleFries>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                /* Large Sides */
+                item => {
+                    Assert.IsType<VokunSalad>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<FriedMiraak>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<MadOtarGrits>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<DragonbornWaffleFries>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                }
+            );
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectDrinkList()
+        {
+            var drinks = Menu.Drinks();
+            // Run an assertion lambda for each drink
+            Assert.Collection(drinks,
+                /* Small Drinks */
+                item => {
+                    Assert.IsType<AretinoAppleJuice>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<CandlehearthCoffee>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<MarkarthMilk>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<WarriorWater>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Blackberry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Cherry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Grapefruit, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Lemon, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Peach, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Watermelon, ((SailorSoda)item).Flavor);
+                },
+                /* Medium Drinks */
+                item => {
+                    Assert.IsType<AretinoAppleJuice>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<CandlehearthCoffee>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<MarkarthMilk>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<WarriorWater>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Blackberry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Cherry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Grapefruit, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Lemon, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Peach, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Watermelon, ((SailorSoda)item).Flavor);
+                },
+                /* Large Drinks */
+                item => {
+                    Assert.IsType<AretinoAppleJuice>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<CandlehearthCoffee>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<MarkarthMilk>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<WarriorWater>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Blackberry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Cherry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Grapefruit, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Lemon, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Peach, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Watermelon, ((SailorSoda)item).Flavor);
+                }
+            );
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectFullMenu()
+        {
+            var items = Menu.FullMenu();
+            Assert.Collection(items,
+                /* Entrees */
+                item => Assert.IsType<BriarheartBurger>(item),
+                item => Assert.IsType<DoubleDraugr>(item),
+                item => Assert.IsType<ThalmorTriple>(item),
+                item => Assert.IsType<GardenOrcOmelette>(item),
+                item => Assert.IsType<PhillyPoacher>(item),
+                item => Assert.IsType<SmokehouseSkeleton>(item),
+                item => Assert.IsType<ThugsTBone>(item),
+                /* Small Sides */
+                item => {
+                    Assert.IsType<VokunSalad>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<FriedMiraak>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<MadOtarGrits>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<DragonbornWaffleFries>(item);
+                    Assert.Equal(Size.Small, ((Side)item).Size);
+                },
+                /* Medium Sides */
+                item => {
+                    Assert.IsType<VokunSalad>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<FriedMiraak>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<MadOtarGrits>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<DragonbornWaffleFries>(item);
+                    Assert.Equal(Size.Medium, ((Side)item).Size);
+                },
+                /* Large Sides */
+                item => {
+                    Assert.IsType<VokunSalad>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<FriedMiraak>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<MadOtarGrits>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                item => {
+                    Assert.IsType<DragonbornWaffleFries>(item);
+                    Assert.Equal(Size.Large, ((Side)item).Size);
+                },
+                /* Small Drinks */
+                item => {
+                    Assert.IsType<AretinoAppleJuice>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<CandlehearthCoffee>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<MarkarthMilk>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<WarriorWater>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Blackberry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Cherry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Grapefruit, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Lemon, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Peach, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Small, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Watermelon, ((SailorSoda)item).Flavor);
+                },
+                /* Medium Drinks */
+                item => {
+                    Assert.IsType<AretinoAppleJuice>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<CandlehearthCoffee>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<MarkarthMilk>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<WarriorWater>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Blackberry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Cherry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Grapefruit, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Lemon, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Peach, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Medium, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Watermelon, ((SailorSoda)item).Flavor);
+                },
+                /* Large Drinks */
+                item => {
+                    Assert.IsType<AretinoAppleJuice>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<CandlehearthCoffee>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<MarkarthMilk>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<WarriorWater>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Blackberry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Cherry, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Grapefruit, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Lemon, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Peach, ((SailorSoda)item).Flavor);
+                },
+                item => {
+                    Assert.IsType<SailorSoda>(item);
+                    Assert.Equal(Size.Large, ((Drink)item).Size);
+                    Assert.Equal(SodaFlavor.Watermelon, ((SailorSoda)item).Flavor);
+                }
+            );
+        }
+    }
+}
