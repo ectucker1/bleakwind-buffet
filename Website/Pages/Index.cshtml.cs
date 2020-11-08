34688c53e1ca3d0f71ff584b42142e3af4d83077
Ethan Tucker <ectucker1@gmail.com>
2020-11-08 15:42:30 -0600

    Created the filtering layout on the index page

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -13,13 +13,68 @@ using Microsoft.Extensions.Logging;
 */
 namespace BleakwindBuffet.Website.Pages
 {
+    /// <summary>
+    /// Page model representing properties of the index page
+    /// </summary>
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        /// <summary>
+        /// The minimum item price to be searched for
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public double? MinPrice { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        /// <summary>
+        /// The maximum item price to be searched for
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public double? MaxPrice { get; set; }
+
+        /// <summary>
+        /// The minimum number of calories to be searched for
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public double? MinCalories { get; set; }
+
+        /// <summary>
+        /// The maximum number of calories to be searched for
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public double? MaxCalories { get; set; }
+
+        /// <summary>
+        /// Terms to search for
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// True if entrees should be included in results
+        /// </summary>
+        public bool IncludeEntrees { get; set; } = true;
+
+        /// <summary>
+        /// True if sides should be included in results
+        /// </summary>
+        public bool IncludeSides { get; set; } = true;
+
+        /// <summary>
+        /// True if drinks should be included in results
+        /// </summary>
+        public bool IncludeDrinks { get; set; } = true;
+
+        /// <summary>
+        /// Called when a GET request is recieved
+        /// </summary>
+        public void OnGet()
         {
-            _logger = logger;
+            // Don't change checkboxes from default of true if there's no form data
+            if (Request.Query.Count > 0)
+            {
+                IncludeEntrees = Request.Query.ContainsKey(nameof(IncludeEntrees));
+                IncludeSides = Request.Query.ContainsKey(nameof(IncludeSides));
+                IncludeDrinks = Request.Query.ContainsKey(nameof(IncludeDrinks));
+            }
         }
     }
 }
